3f07e3212e964b3c836dd44a3930cd9aa15730f9
Meghana754 <meghanarajasekharlingala@gmail.com>
2024-12-03 14:45:05 +0530

    Update student.java

---
+++ student.java
@@ -0,0 +1,26 @@
+class student
+  {
+    string name;
+    int roll number;
+    string course;
+    student(string n,int n,string c)
+    {
+      this.name=n;
+      this.roll no=r;
+      this.course=c;
+    }
+    public void display()
+    {
+      system.out.println("Name is:" +this.name);
+      system.out.println("Roll number is:" +this.roll number);
+      system.out.println("course is:" +this.course);
+    }
+  }
+public class Sampleclass
+  {
+    public static void main(String args[])
+    {
+      student s1=new student("meghana",79,"MCA");
+      s1.display;
+    }
+  }
